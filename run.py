8fcd4244ad89ef4a96aff3b551f12295dc45ac15
Pierre Mesure <pierre@mesu.re>
2023-03-29 12:05:24 +0200

    Sorting the data to make it easier to visualise diff in the future

--- run.py
+++ run.py
@@ -2,6 +2,9 @@ import mprt
 from services.writer import Writer
 
 register = mprt.get_all_pages()
+register = sorted(register, key=lambda d: d['name'])
+register = sorted(register, key=lambda d: d['owner'])
+register = sorted(register, key=lambda d: d['type'])
 
 Writer.write_csv(register, 'register.csv')
 Writer.write_json(register, 'register.json')
